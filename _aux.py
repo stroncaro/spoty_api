64087148b378dec46ef9f230a71e74f19500bf96
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2024-09-26 12:47:17 -0300

    Refactor validate_json required_fields into keyword argument for code clarity

--- _aux.py
+++ _aux.py
@@ -3,7 +3,7 @@ from typing import Iterable
 from .exceptions import InvalidJSONException
 
 
-def validate_json(json: dict, required_fields: Iterable[str]):
+def validate_json(json: dict, *, required_fields: Iterable[str]):
     """Raise InvalidJSONException if `json` doesn't have all required fields"""
     missing_fields = (field for field in required_fields if field not in dict.keys())
     if missing_fields:
