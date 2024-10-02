d0737a954ce2ba6637b37b74c635bab3061fcd84
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2024-10-02 02:49:36 -0300

    Refactor utility methods out of with_authorization_code

--- spoty_api/client/SpotifyClient.py
+++ spoty_api/client/SpotifyClient.py
@@ -31,17 +31,17 @@ class SpotifyClient(
 
     @staticmethod
     def _request_access_token(headers, payload):
-        response = requests.post(
-            TOKEN,
-            headers=headers,
-            data=payload,
-        )
+        response = requests.post(TOKEN, headers=headers, data=payload)
+        SpotifyClient._validate_access_token_response(response)
+        auth_data = SpotifyWebAPIAuthorizationData(data=response.json())
+        return auth_data.access_token
+
+    @staticmethod
+    def _validate_access_token_response(response: requests.Response):
         if not response.status_code == 200:
             raise InvalidClientCredentialsException(
                 f"Server response: {response.json()}"
             )
-        auth_data = SpotifyWebAPIAuthorizationData(data=response.json())
-        return auth_data.access_token
 
     @staticmethod
     def _generate_client_credentials_headers():
@@ -82,6 +82,50 @@ class SpotifyClient(
             "redirect_uri": redirect_uri,
         }
 
+    @staticmethod
+    def _generate_credentials_request(
+        client_id: str,
+        redirect_uri: str,
+        state: str,
+        scope: List[str],
+        show_dialog: bool,
+    ):
+        payload = {
+            "grant_type": "client_credentials",
+            "client_id": client_id,
+            "response_type": "code",
+            "redirect_uri": redirect_uri,
+        }
+        if state:
+            payload["state"] = state
+        if scope:
+            payload["scope"] = " ".join(scope)
+        if show_dialog:
+            payload["show_dialog"] = "true"
+        auth_page = requests.get(
+            AUTHORIZE,
+            params=payload,
+        )
+        return auth_page.url
+
+    @staticmethod
+    def _get_code_from_redirect(auth_result, redirect_uri):
+        if not auth_result.startswith(redirect_uri):
+            raise ValueError(
+                f"Input does not correspond with redirect_uri:\n  {redirect_uri = }\n  {auth_result  = }"
+            )
+
+        # TODO: Validate state
+
+        auth_response_query = requests.utils.urlparse(auth_result).query
+        auth_response_params = dict(
+            q.split("=") for q in auth_response_query.split("&")
+        )
+        if "error" in auth_response_params:
+            raise ValueError(f"Log in failed: {auth_response_params=}")
+        code = auth_response_params["code"]
+        return code
+
     @classmethod
     def with_authorization_code(
         cls: Type[Self],
@@ -93,27 +137,9 @@ class SpotifyClient(
         scope: List[str] = [],
         show_dialog: bool = False,
     ):
-        # TODO: Cleanup, possibly creating utility methods
 
-        payload = {
-            "grant_type": "client_credentials",
-            "client_id": client_id,
-            "response_type": "code",
-            "redirect_uri": redirect_uri,
-        }
-
-        if state:
-            payload["state"] = state
-
-        if scope:
-            payload["scope"] = " ".join(scope)
-
-        if show_dialog:
-            payload["show_dialog"] = "true"
-
-        auth_page = requests.get(
-            AUTHORIZE,
-            params=payload,
+        login_url = cls._generate_credentials_request(
+            client_id, redirect_uri, state, scope, show_dialog
         )
 
         # TODO: Open a browser and capture the redirection after user logs in to finish log in protocol
@@ -121,24 +147,10 @@ class SpotifyClient(
         #   Playwright - https://playwright.dev/python/docs/intro
         #   Selenium - https://www.selenium.dev/documentation/webdriver/getting_started/install_library/
 
-        print(f"Please open the following URL and log in: {auth_page.url}")
+        print(f"Please open the following URL and log in: {login_url}")
         print("When you are done, paste the URL you've been redirected to here:")
-        auth_result = input()
-
-        if not auth_result.startswith(redirect_uri):
-
-            raise ValueError(
-                f"Input does not correspond with redirect_uri:\n  {redirect_uri = }\n  {auth_result  = }"
-            )
-
-        auth_response_query = requests.utils.urlparse(auth_result).query
-        auth_response_params = dict(
-            q.split("=") for q in auth_response_query.split("&")
-        )
-        if "error" in auth_response_params:
-            raise ValueError(f"Log in failed: {auth_response_params=}")
-        code = auth_response_params["code"]
-
+        redirect_result = input()
+        code = cls._get_code_from_redirect(redirect_result, redirect_uri)
         headers = cls._generate_authorization_code_headers(client_id, client_secret)
         payload = cls._generate_authorization_code_payload(code, redirect_uri)
         access_token = cls._request_access_token(headers, payload)
